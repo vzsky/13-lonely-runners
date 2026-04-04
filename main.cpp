abd3a6d0f1812aa956f2ebcbce85b0b3c6903e4f
Touch Sungkawichai <my999n@gmail.com>
2026-04-04 09:53:58 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -175,53 +175,67 @@ int main()
   constexpr auto Print        = [] { return Config{Config::Type::Print, 0}; };
   constexpr auto LargeResolve = [] { return Config{Config::Type::LargeResolve, 0}; };
 
-  // constexpr int K             = 8;
-  // constexpr std::array primes = {47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97,  101, 103,
-  //                                107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
-  //                                179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241};
-  // constexpr std::array config = {Squeeze(2), Force(3), Force(3)};
+  constexpr int K = 8;
 
-  // constexpr int K             = 9;
-  // constexpr std::array primes = {19,  53,  59,  67,  71,  73,  79,  83,  89,  97,  101, 103, 107,
-  //                                109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
-  //                                181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251,
-  //                                257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317};
-  // constexpr std::array config = {Squeeze(2), Squeeze(3), Force(2), Force(5)};
+  if constexpr (K == 8)
+  {
+    constexpr std::array primes = {47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97,  101, 103,
+                                   107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                                   179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241};
+    constexpr std::array config = {Squeeze(2), Force(3), Force(3)};
+    roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  }
 
-  // constexpr int K             = 10;
-  // constexpr std::array primes = {
-  //     103, 107, 109, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
-  //     223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
-  //     337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449,
-  //     457};
-  // constexpr std::array config = {Squeeze(2), Squeeze(3), Print()};
+  if constexpr (K == 9)
+  {
+    constexpr std::array primes = {19,  53,  59,  67,  71,  73,  79,  83,  89,  97,  101, 103, 107,
+                                   109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
+                                   181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251,
+                                   257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317};
+    constexpr std::array config = {Squeeze(2), Squeeze(3), Force(2), Force(5)};
+    roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  }
 
-  constexpr int K             = 11;
-  constexpr std::array primes = {
-      23,  131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227,
-      229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337,
-      347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449,
-      457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577};
-  constexpr std::array config = {Force(2), Force(2), Squeeze(2), Force(2), Force(2), Force(3), Maybe(3)};
+  if constexpr (K == 10)
+  {
+    constexpr std::array primes = {103, 107, 109, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                                   179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241,
+                                   251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
+                                   331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
+                                   409, 419, 421, 431, 433, 439, 443, 449, 457};
+    constexpr std::array config = {Squeeze(2), Squeeze(3), LargeResolve(), Print()};
+    roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  }
 
-  // constexpr int K             = 12;
-  // constexpr std::array primes = {
-  // confirmed
-  // 139, 149, 151, 167, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 251, 257, 263, 269,
-  // 271, 277, 281, 283, 307, 311, 313, 347, 379, 433, 241, 293, 317, 331, 337, 349, 353, 359,
-  // 367, 373, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443,
-  // 503, 509, 521, 523, 571, 577, 613, 617, 619 463,
-  // 541, 547, 557, 599, 601, 607, 631, 641, 659, 661, 677, 683
-  // };
-  // constexpr std::array config = {Force(2), Force(2), Project(2), Force(3), Project(3), Print()};
-  // constexpr std::array config = {Squeeze(2), Squeeze(3), Print()};
+  if constexpr (K == 11)
+  {
+    constexpr std::array primes = {
+        23,  131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227,
+        229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337,
+        347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449,
+        457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577};
+    constexpr std::array config = {Force(2), Force(2), Squeeze(2), Force(2), Force(2), Force(3), Maybe(3)};
+    roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  }
 
-  // constexpr int K             = 13;
-  // constexpr std::array primes = {199, 211, 223, 227,
-  //                                229, 233, 239, 251, 257, 263, 269, 271, 277, 281, 283, 307, 311, 313,
-  //                                347, 379, 433, 439, 443, 449, 457, 461, 241, 293, 293, 317, 331, 337,
-  //                                349, 353, 359, 367, 373, 383, 389, 397, 401, 409, 419, 421, 431};
-  // constexpr std::array config = {Force(2), Force(2), Project(2), Force(3), Project(3), Print()};
+  if constexpr (K == 12)
+  {
+    constexpr std::array primes = {139, 149, 151, 167, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,
+                                   239, 251, 257, 263, 269, 271, 277, 281, 283, 307, 311, 313, 347, 379, 433,
+                                   241, 293, 317, 331, 337, 349, 353, 359, 367, 373, 383, 389, 397, 401, 409,
+                                   419, 421, 431, 433, 439, 443, 503, 509, 521, 523, 571, 577, 613, 617, 619,
+                                   541, 547, 557, 599, 601, 607, 631, 641, 659, 661, 677, 683};
+    constexpr std::array config = {Squeeze(2), Squeeze(3), LargeResolve(), Print()};
+    roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  }
 
-  roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  if constexpr (K == 12)
+  { // wip
+    constexpr std::array primes = {199, 211, 223, 227, 229, 233, 239, 251, 257, 263, 269, 271, 277, 281, 283,
+                                   307, 311, 313, 347, 379, 433, 439, 443, 449, 457, 461, 241, 293, 293, 317,
+                                   331, 337, 349, 353, 359, 367, 373, 383, 389, 397, 401, 409, 419, 421, 431};
+    constexpr std::array config = {Force(2), Force(2), Force(2),  Project(),
+                                   Force(3), Force(3), Project(), Print()};
+    roll_works<K, primes, config>(std::make_index_sequence<primes.size()>{});
+  }
 }
